0c0ffe3f158096e50a4eaf10a1824961693299d1
carole-lavillonniere <carole.lavillonniere@gmail.com>
2021-12-03 10:11:56 +0100

    fix fzf config

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,10 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/carolavillo/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/carolavillo/.fzf/bin"
-fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/carolavillo/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/carolavillo/.fzf/shell/key-bindings.bash"
+source "/usr/share/doc/fzf/examples/key-bindings.bash"
